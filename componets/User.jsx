8192d8095c21e129c38bf04442dec659c0f65422
hardik7219 <hendreogaming@gmail.com>
2026-05-08 01:42:58 +0530

    fix

--- componets/User.jsx
+++ componets/User.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 function User({name}) {
   return (
     <>
-        <div className='border p-1'>
+        <div className='border rounded-lg p-1'>
             <div className='flex items-center gap-4'>
-                <div className='bg-amber-50 h-10 w-10 rounded-full'>i</div>
                 <div>
                     <p>{name}</p>
                 </div>
